7639f51f70e652c38fbd13a29444d00032e9471f
HulkMeyer <coltmeyer6883@hotmail.com>
2026-05-04 15:52:58 -0500

    First stab at problem.  currently returns null instead of correct true or false.

--- Contains Duplicates/Scripts/Solution_1_contains_duplicates.py
+++ Contains Duplicates/Scripts/Solution_1_contains_duplicates.py
@@ -4,3 +4,20 @@
     same then I can move on to the next number and repeat the process until I have gone through the whole list.
     If I have gone through the whole list and have not found any duplicates then I can return false.
     """
+    
+    class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(0, len(nums)-1):
+            if nums[i] == nums[i-1]:
+                return True
+            elif nums[i] == nums[i+1]:
+                return True
+            else:
+                return False
+            
+            
+        """
+        Presently does not work because it is only checking the first number and not the rest of the numbers in
+        the list.
+        """
