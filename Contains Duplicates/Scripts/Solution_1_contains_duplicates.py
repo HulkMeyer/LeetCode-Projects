81eb2281bbf795565ea87c1d0b6d508eb69052d1
HulkMeyer <coltmeyer6883@hotmail.com>
2026-05-04 14:37:05 -0500

    Starting first script for contains duplicates.  Started with the approach that I plan to take.

---
+++ Contains Duplicates/Scripts/Solution_1_contains_duplicates.py
@@ -0,0 +1,6 @@
+    """
+    To solve this problem first I want to sort the list of numbers.  Then I want to check the value above and 
+    below it to see if they are the same.  If they are the same then I can return true.  If they are not the 
+    same then I can move on to the next number and repeat the process until I have gone through the whole list.
+    If I have gone through the whole list and have not found any duplicates then I can return false.
+    """
